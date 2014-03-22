659a2547f9bd21ac6fdb92dac2bc8458502da33e
Ilan Biala <ilan.biala@gmail.com>
2014-03-22 18:13:44 -0400

    testing the accelerometer

---
+++ public/scripts/main.js
@@ -0,0 +1,7 @@
+window.ondevicemotion = function(event) {  
+    var accelerationX = event.accelerationIncludingGravity.x;  
+    var accelerationY = event.accelerationIncludingGravity.y;  
+    var accelerationZ = event.accelerationIncludingGravity.z;  
+    var string = 'accelerationX :' + accelerationX + '<br>' + 'accelerationY: ' + accelerationY + '<br>' + 'accelerationZ: ' + accelerationZ;
+    $('.accel').text(string);
+} 
