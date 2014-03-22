b6ccc2e97ef1e0c88e7ee0678feab9d7d67a6689
Ilan Biala <ilan.biala@gmail.com>
2014-03-22 18:44:25 -0400

    using standard acceleration and motion APIs

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,36 @@
-window.ondevicemotion = function(event) {  
-    var accelerationX = event.accelerationIncludingGravity.x;  
-    var accelerationY = event.accelerationIncludingGravity.y;  
-    var accelerationZ = event.accelerationIncludingGravity.z;  
-    var string = 'accelerationX :' + accelerationX + '<br>' + 'accelerationY: ' + accelerationY + '<br>' + 'accelerationZ: ' + accelerationZ;
-    $('.accel').text(string);
-};
+$(document).ready(function() {
+	if (!(window.DeviceOrientationEvent && window.DeviceMotionEvent)) {
+
+	} else {
+		window.addEventListener('devicemotion', deviceMotionHandler, false);
+
+		function deviceMotionHandler(eventData) {
+			var info, xyz = "[X, Y, Z]";
+
+			// Grab the acceleration from the results
+			var acceleration = eventData.acceleration;
+			info = xyz.replace("X", acceleration.x);
+			info = info.replace("Y", acceleration.y);
+			info = info.replace("Z", acceleration.z);
+			$('.accel').text(info);
+
+			// Grab the acceleration including gravity from the results
+			acceleration = eventData.accelerationIncludingGravity;
+			info = xyz.replace("X", acceleration.x);
+			info = info.replace("Y", acceleration.y);
+			info = info.replace("Z", acceleration.z);
+			$('.accelGrav').text(info);
+
+			// Grab the rotation rate from the results
+			var rotation = eventData.rotationRate;
+			info = xyz.replace("X", rotation.alpha);
+			info = info.replace("Y", rotation.beta);
+			info = info.replace("Z", rotation.gamma);
+			$('.accelRotate').text(info);
+
+			// // Grab the refresh interval from the results
+			info = eventData.interval;
+			$('.accel').text(info);
+		}
+	}
+});
