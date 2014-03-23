76c0595c7f9a52a05d5319d26f279bd3ae64a948
Matthew Ramina <mattramina@gmail.com>
2014-03-22 20:44:33 -0400

    merge join_room

--- geodata.js
+++ geodata.js
@@ -6,22 +6,14 @@ var receive = function(data) {
 }
 
 var join_room = function(data) {
-	if (sockets.clients(data.room) < 2) {
+	if (rooms.indexOf(data.room) === -1) {
+		rooms.push(data.room);
+		socket.join(data.room);
+	} else if (sockets.clients(data.room) < 2) {
 		socket.join(data.room);
 	} else {
 		socket.emit('error', {
 			msg: 'room full'
-		});
-	}
-}
-
-var create_room = function(data) {
-	if (rooms.indexOf(data.room) === -1) {
-		rooms.push(data.room);
-		socket.join(data.room);
-	} else {
-		socket.emit('error', {
-			msg: 'room name used'
 		});
 	}
 }
