b4e89c00f0ad55557597dcf406de045c83e80acf
Ilan Biala <ilan.biala@gmail.com>
2014-03-22 20:41:24 -0400

    reformatting

--- geodata.js
+++ geodata.js
@@ -1,23 +1,27 @@
 var rooms = [];
 
-var receive = function(data){	
+var receive = function(data) {
 	var geodata = {};
 	console.log(data);
 }
 
-var join_room = function(data){
-	if (sockets.clients(data.room) < 2){
+var join_room = function(data) {
+	if (sockets.clients(data.room) < 2) {
 		socket.join(data.room);
-	} else{
-		socket.emit('error', {msg: 'room full'});
+	} else {
+		socket.emit('error', {
+			msg: 'room full'
+		});
 	}
 }
 
-var create_room = function(data){
-	if (rooms.indexOf(data.room) === -1){
+var create_room = function(data) {
+	if (rooms.indexOf(data.room) === -1) {
 		rooms.push(data.room);
 		socket.join(data.room);
 	} else {
-		socket.emit('error', {msg: 'room name used'});
+		socket.emit('error', {
+			msg: 'room name used'
+		});
 	}
 }
