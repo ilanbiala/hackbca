cd46404a2673bfde4d49f7a4b6005d8eb4500108
Ilan Biala <ilan.biala@gmail.com>
2014-03-23 02:19:17 -0400

    fixed spelling

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -69,7 +69,7 @@ function startGame() {
 
 function success(position) {
 	currentLocation.x = Math.pi/180*6367449*Math.cos(position.coords.longitude);
-	currentLocation.y = 111132.954-559.822*Math.cos(2*postion.coords.latitude)+1.175*Math.cos(4*postion.coords.latitude);
+	currentLocation.y = 111132.954-559.822*Math.cos(2*position.coords.latitude)+1.175*Math.cos(4*position.coords.latitude);
 	currentLocation.speed = position.coords.speed;
 	currentLocation.accuracy = position.coords.accuracy;
 	history.push(currentLocation);
