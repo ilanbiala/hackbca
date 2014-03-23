10637fdf46fe5bc2ad095c3404aba821987146da
Matthew Ramina <mattramina@gmail.com>
2014-03-23 04:43:10 -0400

    tokbox take 3

--- tok/tok.py
+++ tok/tok.py
@@ -5,5 +5,5 @@ api_secret = "c196cd873a254b3f92b6efbcaab769c43729034f"
  
 opentok_sdk = OpenTokSDK.OpenTokSDK(api_key, api_secret)
 session = opentok_sdk.create_session()
-token = opentok_sdk.generate_token()
+token = opentok_sdk.generate_token(session.session_id)
 print session.session_id + '$' + token
