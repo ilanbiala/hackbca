8cf9e300dbedfb33cbcd304933d4cf516124ddd9
Matthew Ramina <mattramina@gmail.com>
2014-03-22 20:58:41 -0400

    redirects to room url

--- geodata.js
+++ geodata.js
@@ -9,8 +9,12 @@ var join_room = function(data) {
 	if (rooms.indexOf(data.room) === -1) {
 		rooms.push(data.room);
 		socket.join(data.room);
+		socket.emit('room_joined', {room: data.room});
+		res.redirect('/room/'+data.room);
 	} else if (sockets.clients(data.room) < 2) {
 		socket.join(data.room);
+		socekt.emit('room_joined', {room: data.room});
+		res.redirect('/room/'+data.room);
 	} else {
 		socket.emit('error', {
 			msg: 'room full'
